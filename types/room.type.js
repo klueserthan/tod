195c94361b9acd07a28107eb1d8f8a231f8892e4
bobrae <braegger.bodo@gmail.com>
2021-09-13 23:24:02 +0200

    remove compiled.js files from git

--- types/room.type.js
+++
@@ -1,7 +0,0 @@
-export var ModerationType;
-(function (ModerationType) {
-    ModerationType[ModerationType["Ban"] = 0] = "Ban";
-    ModerationType[ModerationType["Flag"] = 1] = "Flag";
-    ModerationType[ModerationType["Remove"] = 2] = "Remove";
-})(ModerationType || (ModerationType = {}));
-//# sourceMappingURL=room.type.js.map
